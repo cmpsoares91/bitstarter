ad75b487cd8db75c4a4e23ee7d9d1cc8a5dc319b
Carlos Soares <cmpsoares91@gmail.com>
2013-07-08 02:46:00 +0000

    I guess these are the prerequesites for part one of hw3.

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var output = fs.readFileSync(infile, 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
